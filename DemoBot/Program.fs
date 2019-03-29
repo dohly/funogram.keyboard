9d962f363dd1998a1baa7a7b33121f5a11dc4470
Ivan <i.marinin@plumsail.com>
2019-03-29 21:39:04 +0500

    Demo bot added

---
+++ DemoBot/Program.fs
@@ -0,0 +1,74 @@
+﻿module Funogram.TestBot
+
+open System.IO
+open Funogram.Api
+open Funogram.Types
+open Funogram.Bot
+open Funogram.Types
+open FunHttp
+open System.Net.Http
+
+
+[<Literal>]
+let TokenFileName = "token"
+let mutable botToken = "none"
+
+let processMessageBuild config =
+
+    let defaultText = """⭐️Keyboard demo bot:
+    /calendar - Calendar keyboard example
+    /choice - Choice keyboard example"""
+
+
+    let processResultWithValue (result: Result<'a, ApiResponseError>) =
+        match result with
+        | Ok v -> Some v
+        | Error e ->
+            printfn "Error: %s" e.Description
+            None
+
+    let processResult (result: Result<'a, ApiResponseError>) =
+        processResultWithValue result |> ignore
+
+    let botResult data = api config data |> Async.RunSynchronously
+    let bot data = botResult data |> processResult
+
+    let updateArrived ctx =
+        let fromId() = ctx.Update.Message.Value.From.Value.Id        
+        let sendMessageFormatted text parseMode = (sendMessageBase (ChatId.Int(fromId())) text (Some parseMode) None None None None) |> bot
+
+        let notHandled =
+            processCommands ctx [
+                cmd "/calendar"  (fun _ -> sendMessageFormatted "Calendar Demo" ParseMode.Markdown)
+            ]
+        if notHandled then bot (sendMessage (fromId()) defaultText)
+    updateArrived
+
+let start token =
+    (*
+    * Set poxy
+    *```fsharp
+    * let handler = new HttpClientHadler ()
+    * handler.Proxy <- createMyProxy ()
+    * handler.UseProxy <- true
+    * let config = { defaultConfig with Token = token
+    *                                   Cleint = new HttpClient(handler, true) }
+    *```
+    *)
+    let config = { defaultConfig with Token = token }
+    let updateArrived = processMessageBuild config
+    startBot config updateArrived None
+
+[<EntryPoint>]
+let main argv =
+    printfn "Bot started..."
+    let startBot = 
+        if File.Exists(TokenFileName) then
+            start (File.ReadAllText(TokenFileName))
+        else
+            printf "Please, enter bot token: "
+            let token = System.Console.ReadLine()
+            File.WriteAllText(TokenFileName, token)
+            start token
+    startBot |> Async.RunSynchronously
+    0
