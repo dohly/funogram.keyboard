5d65bef60fa9c4c53489ac82c926480791a230dc
Ivan <i.marinin@plumsail.com>
2019-03-29 22:11:12 +0500

    Base library is done

---
+++ Funogram.Keyboard/Optional.fs
@@ -0,0 +1,15 @@
+﻿namespace Funogram
+[<AutoOpen>]
+module internal Optional=
+  type OptionalBuilder() =
+    member __.Bind(opt, binder) =
+      match opt with
+      | Some value -> binder value
+      | None -> None
+    member __.Return(value) =
+      Some value
+    member __.ReturnFrom(value) =
+      __.Bind(value, Some)
+    member __.Zero()=None
+      
+  let optional = new OptionalBuilder()
